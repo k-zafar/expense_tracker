196f9ebb6d4601740c8bd1e7937893d6256f7abc
Kamran Zafar <kamran.zafar@cognitiveaxis.com>
2019-09-03 11:45:01 +0500

    KRT-3, Adding test for expense model.

--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@expense = expenses(:expense)
+	end
+
+	test "valid expense" do
+		assert @expense.valid?
+	end
+
+	test "invalid without amount" do
+		@expense.amount = nil
+		refute @expense.valid?, "amount is required"
+    	assert_not_nil @expense.errors[:amount], "Error"
+	end
+
+	test "invalid without description" do
+		@expense.description = nil
+		refute @expense.valid?
+    	assert_not_nil @expense.errors[:description]
+	end
+
+	test "invalid without user" do
+		@expense.user = nil
+		refute @expense.valid?
+    	assert_not_nil @expense.errors[:user]
+	end
 end
